8844b525aa45f526d4321db5dcc0a39a0a87b890
Nacho Bartomeus <nacho.bartomeus@gmail.com>
2026-09-18 09:35:53 +0200

    Add data and scripts

---
+++ scripts/install_packages.R
@@ -0,0 +1,6 @@
+#this script install all packges used in the workshop
+
+install.packages("readr")
+install.packages("renv")
+install.packages("devtools")
+devtools::install_github("ropenscilabs/dataspice")
